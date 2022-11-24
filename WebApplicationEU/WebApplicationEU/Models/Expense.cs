80ae1190b1dcb9911fcba4afb3d7c5f7179be631
Tim4r <gendulf_zdes@mail.ru>
2022-11-24 18:46:15 +0300

    Progect CRUD on ASP.NET

---
+++ WebApplicationEU/WebApplicationEU/Models/Expense.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationEU.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Expense Name")]
+        [Required]
+        public string ExpenseName { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Amout must be greater then zero!")]
+        public int Amount { get; set; }
+    }
+}
