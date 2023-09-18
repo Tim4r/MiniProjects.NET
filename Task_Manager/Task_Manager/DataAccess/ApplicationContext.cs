3ffa4eca5e26751cbe19c1161b791459e316abb1
Timur <gendulf_zdes@mail.ru>
2023-09-18 17:48:58 +0300

    First commit

---
+++ Task_Manager/Task_Manager/DataAccess/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task_Manager.Entities;
+
+namespace Task_Manager.Connection
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Category> Categories => Set<Category>();
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-FQLO9UO\\SQLEXPRESS");
+        }
+    }
+}
