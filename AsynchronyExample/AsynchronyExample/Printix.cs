978c9339076d0125629a5fe14dbef154e1ab320b
Timur <43003761+Tim4r@users.noreply.github.com>
2024-01-14 23:44:55 +0300

    Example of using asynchrony

---
+++ AsynchronyExample/AsynchronyExample/Printix.cs
@@ -0,0 +1,17 @@
+﻿namespace AsynchronyExample;
+
+internal class Printix
+{ 
+    internal void Print()
+    {
+        Thread.Sleep(3000);
+        Console.WriteLine($"Hello! [{Thread.CurrentThread.ManagedThreadId}]");
+    }
+
+    internal async void PrintAsync()
+    {
+        Console.WriteLine($"Начало работы PrintAsync [{Thread.CurrentThread.ManagedThreadId}]");
+        await Task.Run(Print);
+        Console.WriteLine($"Конец метода PrintAsync [{Thread.CurrentThread.ManagedThreadId}]");
+    }
+}
---
+++ AsynchronyExample/AsynchronyExample/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AsynchronyExample;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Printix printix = new Printix();
+        printix.PrintAsync();
+
+        Console.WriteLine($"Продолжаем работу в методе Main [{Thread.CurrentThread.ManagedThreadId}]");
+        Thread.Sleep(5000);
+        Console.WriteLine($"Завершаем работу Main [{Thread.CurrentThread.ManagedThreadId}]");
+    }
+}
