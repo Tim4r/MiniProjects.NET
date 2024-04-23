26cd87599c0f537a9d283e2e139f71d82accf6c7
Tim4r <gendulf.zdes@mail.ru>
2024-04-23 21:36:41 +0300

    Learning anonymous method and delegate

--- EventDelegateLambda/AnonymousMethod/Program.cs
+++ EventDelegateLambda/AnonymousMethod/Program.cs
@@ -6,20 +6,40 @@ internal class Program
 
     public delegate void AbraCadabra(string abra);
 
+    delegate void MessageHandler(string message);
+
+    delegate void MessageHandler2(int num);
+
     static void Main(string[] args)
     {
+        // Without parameters
         Kinsasa Kuplinov = delegate ()
         {
             Console.WriteLine("AnonymousWithoutParameters");
         };
         Kuplinov();
 
-        //////////////////////////////////////////////////////////////////////
-
+        // With string parameter
         AbraCadabra Lololoshka = delegate (string abra)
         {
             Console.WriteLine(abra);
         };
         Lololoshka("AnonymousAndParameters");
+
+        // With string parameter, but shorter
+        MessageHandler handler = Console.WriteLine;
+        handler("AnonymousAndParametersShorter");
+
+        // Passing an anonymous method as a argument
+        ShowMessage(Convert.ToInt32(Console.ReadLine()), delegate (int num)
+        {
+            Console.WriteLine(num*2);
+            Console.WriteLine("You are a hero!");
+        });
+    }
+
+    static void ShowMessage(int number, MessageHandler2 handler2)
+    {
+        handler2(number);
     }
 }
---
+++ EventDelegateLambda/Delegate/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Delegates
+{
+    internal class Program
+    {
+        delegate void MessageHandler2(int num);
+        static void Main()
+        {
+            ShowMessage(Convert.ToInt32(Console.ReadLine()), new(KyKaReKy));
+        }
+
+        // Passing a delegate as a parameter
+        static void ShowMessage(int number, MessageHandler2 handler2)
+        {
+            handler2(number);
+        }
+
+        static void KyKaReKy(int num)
+        {
+            Console.WriteLine(num * 2);
+            Console.WriteLine("You are a hero!");
+        }
+    }
+}
