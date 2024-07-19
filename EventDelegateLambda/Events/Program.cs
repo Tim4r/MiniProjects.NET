09b5e17c66721dbd1b7e4017125514de19cd0922
Tim4r <yesis.timur@gmail.com>
2024-07-19 14:48:48 +0300

    DelegateCommit

---
+++ EventDelegateLambda/Events/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Events;
+
+delegate void Handler(string alpha, int beta);
+
+internal class Program
+{
+    static event Handler Stuck;                          //!!! We could remove the keyword "event" and nothing would change
+
+    static void Main(string[] args)
+    {
+        Stuck += ShowPrice;                             //Registrating method in an Event
+        Stuck("Text", 100);
+    }
+
+    static void ShowPrice(string alpha, int beta)
+    {
+        Console.WriteLine($"alpha: {alpha} beta: {beta}");
+    }
+}
